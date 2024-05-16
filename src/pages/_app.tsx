d83a16b037930705efd0d59f8718aad900c4ebcb
Zuzanna Mila <zuza.mila@onet.pl>
2024-05-16 11:53:20 +0200

    boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="w-9/12 m-auto pt-10">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
---
+++ src/pages/cart.tsx
@@ -0,0 +1,12 @@
+const Cart = () => {
+    return (
+        <>
+        <h1 className="text-xl font-bold">Total: ${/*to be implemeted */}</h1>
+        <div>
+       
+        </div>
+        </>
+    );
+}
+
+export default Cart;
